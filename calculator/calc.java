c1d43bb0eb1ccc5d7b221a2ecd6c3616d330a0a3
Manabe-kun <48487906+Manabe-kun@users.noreply.github.com>
2019-03-17 21:52:15 +0530

    Create calc.java

---
+++ calculator/calc.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class calc{
+public static void main(String[] daf){
+Sacnner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=s.nextInt();
+System.out.format("the addition : %d",(a+b));
+System.out.format("the Minus : %d",(a-b));
+System.out.format("the multiply : %d",(a*b));
+}
+}
